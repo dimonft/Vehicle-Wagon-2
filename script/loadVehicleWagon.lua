bc65b00981023b83205e26afb15c7a0b3699eeb6
robot256 <robot256@GMAIL.COM>
2020-12-16 21:12:15 -0500

    Work around GCKI interface error
    - GCKI only works with cars, not spider-vehicles.

--- script/loadVehicleWagon.lua
+++ script/loadVehicleWagon.lua
@@ -102,20 +102,23 @@ function loadVehicleWagon(action)
   saveData.burner = saveRestoreLib.saveBurner(vehicle.burner)
   
   -- Store data for other mods
-  if remote.interfaces["autodrive"] and remote.interfaces["autodrive"].get_vehicle_data then
-    -- This will return a table with just { owner = player.index } for now!
-    saveData.autodrive_data = remote.call("autodrive", "get_vehicle_data", vehicle.unit_number, script.mod_name)
-    remote.call("autodrive", "vehicle_removed", vehicle)
-  end
-  if remote.interfaces["GCKI"] and remote.interfaces["GCKI"].get_vehicle_data then
-    -- This will return a table with { owner = player.index, locker = player.index }
-    saveData.GCKI_data = remote.call("GCKI", "get_vehicle_data", vehicle.unit_number)
-    remote.call("GCKI", "vehicle_removed", vehicle, script.mod_name)
-    if saveData.GCKI_data and settings.global["vehicle-wagon-use-GCKI-permissions"].value then
-      if saveData.GCKI_data.owner or saveData.GCKI_data.locker then
-        -- There is an owner or a locker of the vehicle on this wagon.  Make it un-minable.
-        -- GCKI will call an interface function to release it if the owner unclaims it.
-        loaded_wagon.minable = false
+  -- Pi-C Mods only work with type "car", not "spider-vehicle"
+  if vehicle.type == "car" then
+    if remote.interfaces["autodrive"] and remote.interfaces["autodrive"].get_vehicle_data then
+      -- This will return a table with just { owner = player.index } for now!
+      saveData.autodrive_data = remote.call("autodrive", "get_vehicle_data", vehicle.unit_number, script.mod_name)
+      remote.call("autodrive", "vehicle_removed", vehicle)
+    end
+    if remote.interfaces["GCKI"] and remote.interfaces["GCKI"].get_vehicle_data then
+      -- This will return a table with { owner = player.index, locker = player.index }
+      saveData.GCKI_data = remote.call("GCKI", "get_vehicle_data", vehicle.unit_number)
+      remote.call("GCKI", "vehicle_removed", vehicle, script.mod_name)
+      if saveData.GCKI_data and settings.global["vehicle-wagon-use-GCKI-permissions"].value then
+        if saveData.GCKI_data.owner or saveData.GCKI_data.locker then
+          -- There is an owner or a locker of the vehicle on this wagon.  Make it un-minable.
+          -- GCKI will call an interface function to release it if the owner unclaims it.
+          loaded_wagon.minable = false
+        end
       end
     end
   end
